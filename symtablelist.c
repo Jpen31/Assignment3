aa5b25529dd0a3598ad280e574137c8800ba8c00
Jpen <jp3171@princeton.edu>
2022-10-24 16:14:55 -0400

    first edition

--- symtablelist.c
+++ symtablelist.c
@@ -82,7 +82,7 @@ const void *pvValue) {
         return 0;
     }
 
-    *(psNewNode->pcKey) = strcpy((char *) pcKey);
+    strcpy(psNewNode->pcKey, (char *) pcKey);
     psNewNode->pvValue = pvValue;
     psNewNode->psNextBinding = oSymTable->psFirstBinding;
     oSymTable->bindings += 1;
