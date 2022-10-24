ecbe1bbc063ea5697c11092ca951329712f8ac67
Jpen <jp3171@princeton.edu>
2022-10-24 16:23:40 -0400

    first edition

--- testsymtable.c
+++ testsymtable.c
@@ -954,7 +954,7 @@ int main(int argc, char *argv[])
    testKeyComparison();
    testKeyOwnership();
    testRemove();
-   testMap();
+   /* testMap(); */
    testEmptyTable();
    testEmptyKey();
    testNullValue();
