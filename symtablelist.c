8919f7d3d2aba02b0b9615d7a082c2b715578644
Jpen <jp3171@princeton.edu>
2022-10-26 13:45:33 -0400

    first edition

--- symtablelist.c
+++ symtablelist.c
@@ -188,5 +188,17 @@ void *SymTable_remove(SymTable_T oSymTable, const char *pcKey) {
 void SymTable_map(SymTable_T oSymTable,
 void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
 const void *pvExtra) {
-    
+    struct Binding *psCurrentBinding;
+
+    assert(oSymTable != NULL);
+    assert(pcKey != NULL);
+
+    psCurrentBinding = oSymTable->psFirstBinding;
+    while(psNextBinding != NULL) {
+        pfApply(psCurrentBinding->pcKey, psCurrentBinding->pvValue, 
+        pvExtra);
+        psCurrentBinding = psCurrentBinding->psNextBinding;
+    }
+
+    return;
 }
